51f48cf62ea7ac07e57fe3471632b333b7de38ca
Abdulrahman Masoud <78610146+abodmasoud@users.noreply.github.com>
2023-08-02 18:54:35 +0300

    model & migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,6 +23,7 @@ class User extends Authenticatable
         'password',
     ];
 
+
     /**
      * The attributes that should be hidden for serialization.
      *
@@ -42,4 +43,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function books()
+    {
+        return $this->hasMany(Book::class);
+    }
 }
